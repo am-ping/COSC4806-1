36ba29523bb796644d7f16a4ddc635bfb1849437
Arbaaz Mirza <63935001+am-ping@users.noreply.github.com>
2024-05-17 15:58:02 +0000

    Added styling

--- login.php
+++ login.php
@@ -12,27 +12,29 @@ if (isset($_SESSION["authenticated"])) {
     <link rel="stylesheet" type="text/css" href="css/styles.css">
   </head>
 <body>
-
-  <h1>Login Form</h1>
-
-  <form action="/validate.php" method="post">
-    <label for="username">Username:</label>
-    <br>
-    <input type="text" id="username" name="username">
-    <br>
-    <label for="password">Password:</label>
-    <br>
-    <input type="password" id="password" name="password">
-    <br>
-    <input type="submit" value="Submit">
-  </form>
-  <p>
-    <?php 
-    if (isset($_SESSION["failed_attempts"])) {
-      echo "This is unsuccessful attempt number " . $_SESSION["failed_attempts"];
-    }
-    ?>
-  </p>
-
+  <div id="login">
+  
+    <h1>Login</h1>
+  
+    <form action="/validate.php" method="post">
+      <label for="username">Username:</label>
+      <br>
+      <input type="text" id="username" name="username" placeholder="arbaaz">
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password" name="password" placeholder="password">
+      <br>
+      <input type="submit" value="Submit" id="submit">
+    </form>
+    
+  </div>
+  <?php
+  if (isset($_SESSION["failed_attempts"])) {
+    echo '<p id="attempts">';
+    echo "This is unsuccessful attempt number: " . $_SESSION["failed_attempts"];
+    echo '</p>';
+  }
+  ?>
 </body>
 </html>
